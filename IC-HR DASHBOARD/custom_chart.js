ee26b2b28f8ef782262c05142eee12a3125b5f24
Snehasish Char <snehasish.char1995@gmail.com>
2021-12-01 09:42:44 +0530

    profile, dashboard form updated

--- IC-HR DASHBOARD/custom_chart.js
+++ IC-HR DASHBOARD/custom_chart.js
@@ -1,6 +1,41 @@
 window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: "NUMBER OF EMPLOYEES PER DEPARTMENT",
+        },
+        axisX: {
+            interval: 1,
+        },
+        axisY: {
+            includeZero: true,
+            prefix: "$ ",
+        },
+        data: [
+            {
+                type: "column",
+
+                dataPoints: [
+                    { y: 4, label: "Management", color: "blue" },
+                    { y: 10, label: "Account Service", color: "orange" },
+                    { y: 8, label: "Account Planning", color: "lightgray" },
+                    { y: 9, label: "Creative", color: "yellow" },
+                    { y: 4, label: "Finance & Accounts", color: "lightblue" },
+                    { y: 6, label: "Media Buying", color: "lightgreen" },
+                    { y: 14, label: "Production", color: "navy" },
+                    { y: 4, label: "HR & Facillities", color: "brown" },
+                    { y: 4, label: "Research", color: "gray" },
+                    { y: 5, label: "Web Development", color: "olive" },
+                    { y: 4, label: "IT", color: "darkblue" },
+                    { y: 4, label: "Traffic", color: "darkgreen" },
+                ],
+            },
+        ],
+    });
+    chart.render();
+
     // Chart 2: Horzental Bar Chart / AVERAGE SALARY BY DEPARTMENT
-    var chart = new CanvasJS.Chart("chartContainer3", {
+    var chart2 = new CanvasJS.Chart("chartContainer3", {
         animationEnabled: true,
         title: {
             text: "AVERAGE SALARY BY DEPARTMENT",
@@ -33,7 +68,7 @@ window.onload = function () {
             },
         ],
     });
-    chart.render();
+    chart2.render();
 
     // Chart 3: Horzental Bar Chart / Salary Range BreakDown
     var chart1 = new CanvasJS.Chart("chartContainer2", {
@@ -64,6 +99,128 @@ window.onload = function () {
     });
 
     chart1.render();
+
+    //  Chart 4: Horzental Stacked Bar Chart / SALARY BREAKDOWN BY DEPARTMENT
+    var chart4 = new CanvasJS.Chart("chartContainer4", {
+        animationEnabled: true,
+        title: {
+            text: "SALARY BREAKDOWN BY DEPARTMENT",
+        },
+        axisX: {
+            interval: 1,
+        },
+        axisY: {
+            includeZero: true,
+            prefix: "$ ",
+        },
+        data: [
+            {
+                type: "stackedBar",
+                legendText: "Salary",
+                showInLegend: "true",
+                legendMarkerType: "square",
+                legendMarkerColor: "lightblue",
+                dataPoints: [
+                    { y: 4, label: "Traffic", color: "lightblue" },
+
+                    { y: 4, label: "IT", color: "lightblue" },
+
+                    { y: 5, label: "Web Development", color: "lightblue" },
+
+                    { y: 4, label: "Research", color: "lightblue" },
+
+                    { y: 4, label: "HR & Facillities", color: "lightblue" },
+
+                    { y: 14, label: "Production", color: "lightblue" },
+
+                    { y: 6, label: "Media Buying", color: "lightblue" },
+
+                    { y: 6, label: "Media Buying", color: "lightblue" },
+
+                    { y: 4, label: "Finance & Accounts", color: "lightblue" },
+
+                    { y: 9, label: "Creative", color: "lightblue" },
+
+                    { y: 8, label: "Account Planning", color: "lightblue" },
+
+                    { y: 10, label: "Account Service", color: "lightblue" },
+
+                    { y: 4, label: "Management", color: "lightblue" },
+                ],
+            },
+            {
+                type: "stackedBar",
+                legendText: "Bonus",
+                showInLegend: "true",
+                legendMarkerType: "square",
+                legendMarkerColor: "darkblue",
+
+                dataPoints: [
+                    { y: 4, label: "Traffic", color: "darkblue" },
+
+                    { y: 4, label: "IT", color: "darkblue" },
+
+                    { y: 5, label: "Web Development", color: "darkblue" },
+
+                    { y: 4, label: "Research", color: "darkblue" },
+
+                    { y: 4, label: "HR & Facillities", color: "darkblue" },
+
+                    { y: 14, label: "Production", color: "darkblue" },
+
+                    { y: 6, label: "Media Buying", color: "darkblue" },
+
+                    { y: 6, label: "Media Buying", color: "darkblue" },
+
+                    { y: 4, label: "Finance & Accounts", color: "darkblue" },
+
+                    { y: 9, label: "Creative", color: "darkblue" },
+
+                    { y: 8, label: "Account Planning", color: "darkblue" },
+
+                    { y: 10, label: "Account Service", color: "darkblue" },
+
+                    { y: 4, label: "Management", color: "darkblue" },
+                ],
+            },
+            {
+                type: "stackedBar",
+                legendText: "Overtime",
+                showInLegend: "true",
+                legendMarkerType: "square",
+                legendMarkerColor: "orange",
+
+                dataPoints: [
+                    { y: 4, label: "Traffic", color: "orange" },
+
+                    { y: 4, label: "IT", color: "orange" },
+
+                    { y: 5, label: "Web Development", color: "orange" },
+
+                    { y: 4, label: "Research", color: "orange" },
+
+                    { y: 4, label: "HR & Facillities", color: "orange" },
+
+                    { y: 14, label: "Production", color: "orange" },
+
+                    { y: 6, label: "Media Buying", color: "orange" },
+
+                    { y: 6, label: "Media Buying", color: "orange" },
+
+                    { y: 4, label: "Finance & Accounts", color: "orange" },
+
+                    { y: 9, label: "Creative", color: "orange" },
+
+                    { y: 8, label: "Account Planning", color: "orange" },
+
+                    { y: 10, label: "Account Service", color: "orange" },
+
+                    { y: 4, label: "Management", color: "orange" },
+                ],
+            },
+        ],
+    });
+    chart4.render();
 };
 
 // Chart 4: Stacked Bar Chart / SALARY BREAKDOWN BY DEPARTMENT
@@ -165,7 +322,7 @@ var myChart = new Chart(ctx, {
                 backgroundColor: "#5c9ad5",
             },
             {
-                data: [0, 0, 0, 0, 0, 0, 30, 0,0, 0, 90, 0],
+                data: [0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 90, 0],
                 backgroundColor: "#d6844b",
             },
         ],
